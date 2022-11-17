8bbfbdb0306edb1af75dc593cd7395f035b61019
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-17 16:12:19 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,25 @@
+let user = {
+    name: 'Joel',
+}
+// assign()
+// let newUser = Object.assign({}, user, {
+//     surname: 'Mukanya',
+//     email : 'joel@gmail.com'
+// })
+
+// console.log(user);
+// console.log(newUser);
+
+// Spread operator
+// let newUser = {...user};
+// console.log(newUser);
+
+// JSON.parse(JSON.stringify())
+// let newUser = JSON.parse(JSON.stringify(user));
+// console.log(newUser);
+
+// Garbage collection (GC)
+/*
+It purpose is to monitor memory allocation and determine when a 
+block of allocated memory is no longer needed and reclaim it.
+*/
